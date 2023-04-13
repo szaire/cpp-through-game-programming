a89b3f2b4fddbf305e93dcf5702c63c7d93d9762
Samuel Zaire <samuel.zairerocha@gmail.com>
2023-04-12 23:03:20 -0300

    update game with gameplay logic

--- cap6/tic-tac-toe.cpp
+++ cap6/tic-tac-toe.cpp
@@ -1,29 +1,56 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <sys/types.h>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 //
 
 using namespace std;
 
 typedef unsigned short int ushort;
 
+// visuals
 void displayMessage1();
 void displayMessage2();
 void inputFirstMove(char& rfirstMove);
-void renderBoard(vector<ushort>& board);
+void renderBoard(vector<char>& board);
+
+// moves
+void playerMove(vector<char>& board, vector<char>& visitedPos, ushort& position);
+void computer_randomDecision(vector<char>& board, vector<char>& visitedPos);
+
+// end game conditions
+bool drawCondition(vector<char>& board);
 
 int main()
 {
-    vector<ushort> board = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    vector<char> board = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+    vector<char> visitedPos;
     char firstMove;
 
     displayMessage1();
     renderBoard(board);
     displayMessage2();
     inputFirstMove(firstMove);
+    system("clear");
+
+    while (!drawCondition(board))
+    {
+        ushort position;
+        renderBoard(board);
+        if (firstMove == 'y')
+        {
+            playerMove(board, visitedPos, position);
+        }
+        computer_randomDecision(board, visitedPos);
+
+        firstMove = 'y';
+        system("clear");
+    }
 }
 
+// Visuals
 inline void displayMessage1()
 {
     cout << "Welcome to the ultimate man-machine showdown: Tic-Tac-Toe\n";
@@ -42,10 +69,10 @@ void inputFirstMove(char& rfirstMove)
     do {
         cout << "Do you require the first move? (y/n): ";
         cin >> rfirstMove;
-    } while (rfirstMove != 'y' || rfirstMove != 'n');
+    } while (rfirstMove != 'y' && rfirstMove != 'n');
 }
 
-void renderBoard(vector<ushort>& board)
+void renderBoard(vector<char>& board)
 {
     for (int i = 0; i < board.size(); i++) {
         cout << "\t" << board[i] << " ¦ " << board[++i] << " ¦ " << board[++i] << "\n";
@@ -56,3 +83,56 @@ void renderBoard(vector<ushort>& board)
         }
     }
 }
+
+// End game conditions
+// Draw
+bool drawCondition(vector<char>& board)
+{
+    ushort minCharPos = (ushort) '0';
+    ushort maxCharPos = minCharPos + board.size();
+
+    for (int i = 0; i < board.size(); i++) {
+        if ((ushort) board[i] >= minCharPos && (ushort) board[i] < maxCharPos)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Moves
+// Player:
+// TODO: Look for the infinite game bug...
+void playerMove(vector<char>& board, vector<char>& visitedPos, ushort& position)
+{
+    cout << "\nChoose the place to input the symbol: ";
+    cin >> position;
+
+    ushort minCharPos = (ushort) '0';
+    ushort maxCharPos = minCharPos + board.size();
+
+    if ((ushort) board[position] >= minCharPos && (ushort) board[position] < maxCharPos)
+    {
+        board[position] = 'X';
+        visitedPos.push_back(position);
+    }
+}
+
+// AI:
+// Worst decision making algorithm
+void computer_randomDecision(vector<char>& board, vector<char>& visitedPos)
+{
+    srand(static_cast<unsigned int>(time(0)));
+
+    ushort minCharPos = (ushort) '0';
+    ushort maxCharPos = minCharPos + board.size();
+
+    ushort randBoardPos;
+    do {
+        randBoardPos = rand() % board.size();
+    } while (std::count(visitedPos.begin(), visitedPos.end(), randBoardPos));
+
+    board[randBoardPos] = 'O';
+    visitedPos.push_back(randBoardPos);
+}
+
